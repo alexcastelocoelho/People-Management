5b7ac257824f4d389eee8fe826c21f926c9d0602
Alex Castelo <alexcastelo437@gmail.com>
2023-01-21 08:43:02 -0300

    feat: created person model

---
+++ src/app/model/PersonModel.js
@@ -0,0 +1,36 @@
+const {DataTypes} = require("sequelize")
+const database = require("../../database/postgresql/Index.js")
+
+const Person = database.define("Person", {
+    id: {
+        type: DataTypes.INTEGER,
+        autoIncrement: true,
+        allowNull: false,
+        primaryKey: true
+    }, 
+    name: {
+        type: DataTypes.STRING(50),
+        allowNull: false
+    },
+    cpf: {
+        type: DataTypes.STRING(14),
+        allowNull: false
+    },
+    birthday: {
+        type: DataTypes.DATE,
+        allowNull: false
+    },
+    email: {
+        type: DataTypes.STRING(50),
+        allowNull: false
+    },
+    password: {
+        type: DataTypes.STRING(16),
+        allowNull: false
+    }
+}, {timestamps: false})
+
+const person = Person
+
+module.exports = person
+
