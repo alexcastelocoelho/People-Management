9663f01fd839df8022eeb1fe420f43c14e03501c
Alex Castelo <alexcastelo437@gmail.com>
2023-01-21 08:44:30 -0300

    feat: created person controller

---
+++ src/app/controller/PersonController.js
@@ -0,0 +1,51 @@
+const personService = require("../service/PersonService")
+
+class PersonController {
+    async create(req, res ) {
+        try {
+            const result = await personService.create(req.body)
+            return res.status(201).json(result)
+        } catch (error) {
+            return res.status(400).json(error)
+        }
+    }
+
+    async list(req, res) {
+        try {
+            const result = await personService.list(req.query)
+            return res.status(200).json(result)
+        } catch (error) {
+            return res.status(400).json(error) 
+        }
+    }
+
+    async getById(req, res) {
+        try {
+            const result = await personService.getById(req.params.id) 
+            return res.status(200).json(result)
+        } catch (error) {
+            return res.status(400).json(error)
+        }
+    }
+
+    async update(req, res) {
+        try {
+            const result = await personService.update(req.params.id, req.body)
+            return res.status(200).json(result)
+        } catch (error) {
+            return res.status(400).json(error);
+          }
+    }
+
+    async delete(req, res) {
+        try {
+            const result = await personService.delete(req.params.id)
+            return res.status(204).json(result)
+        } catch (error) {
+            return res.status(400).json(error);
+          }
+    }
+}
+
+
+module.exports = new PersonController()
