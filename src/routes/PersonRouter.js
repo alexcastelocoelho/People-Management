3e9a9ff0905a2d92182bbed10d471717f0117a51
Alex Castelo <alexcastelo437@gmail.com>
2023-01-21 12:09:59 -0300

    feat: added person validations in the create and update route

--- src/routes/PersonRouter.js
+++ src/routes/PersonRouter.js
@@ -1,10 +1,11 @@
 const personController = require("../app/controller/PersonController")
-
+const personValidation = require("../app/validations/PersonValidation")
+const personValidateUpdate = require("../app/validations/PersonValidateUpdate")
 module.exports = (server, routes, prefix = "/people" ) => {
-    routes.post("/",  personController.create)
+    routes.post("/",  personValidation ,personController.create)
     routes.get("/", personController.list)
     routes.get("/:id", personController.getById)
-    routes.put("/:id", personController.update)
+    routes.put("/:id", personValidateUpdate, personController.update)
     routes.delete("/:id", personController.delete)
     
     server.use(prefix,routes)
