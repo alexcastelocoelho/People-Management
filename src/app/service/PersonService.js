f7181d4810c97c574798bc65de64794cef19c0d3
Alex Castelo <alexcastelo437@gmail.com>
2023-01-21 08:44:05 -0300

    feat: created person service

---
+++ src/app/service/PersonService.js
@@ -0,0 +1,32 @@
+const personRepository = require("../repository/PersonRepository")
+
+class PersonService {
+    async create(payload) {                   
+        const result = await personRepository.create(payload)
+        return result      
+       
+    }
+
+    async list(payload) {
+        const result = await personRepository.list(payload)
+        return result
+    }
+
+    async getById(id) {
+        const result = await personRepository.getById(id)
+        return result
+    }
+
+    async update(id, payload) {
+        const result = await personRepository.update(id, payload)
+        return result
+    }
+
+    async delete(id) {
+        const result = await personRepository.delete(id)
+        
+    }
+}
+
+
+module.exports = new PersonService()
